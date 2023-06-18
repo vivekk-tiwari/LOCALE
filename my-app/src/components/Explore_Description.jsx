074521d6886ebe8151202db370398482b392e10b
Mayank <myank07official@gmail.com>
2023-06-18 20:52:50 +0530

    updated index.js

--- my-app/src/components/Explore_Description.jsx
+++ my-app/src/components/Explore_Description.jsx
@@ -4,10 +4,15 @@ function ExploreDescription () {
     return (
         <div className="explore_about_main">
             <div className="explore_about">
-                <img src="./images/" alt="" id="explore_img" style={{width:"500px",height:"500px"}} />
+                <img src="./images/img.svg" alt="" id="explore_img" style={{width:"600px",height:"500px"}} />
+                <div className="explore_description">
+                <p id='explore_d_1'>❝ Locale inspires local regional beauty of Chandigarh through showcasing the best of Chandigarh in a unique 
+                style with scratching the deepest parts of the city and finding the best out of it. ❞ </p>
+                </div>
             </div>
         </div>
     );
 }
 
 export default ExploreDescription;
+
