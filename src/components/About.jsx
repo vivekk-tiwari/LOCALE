110dc0fd178f7cee0714b557a7590b50fc66ca3d
Mayank <myank07official@gmail.com>
2023-07-04 12:51:33 +0530

    update About Component

---
+++ src/components/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function About (){
+    return (
+        <>
+        <div className="about_main">
+            <div className="about_heading">
+                <p id="about_text">About Us</p>
+                <p id="about_text_2">Join the conversation !</p>
+            </div>
+            <div className="about_heading_2">
+                <p id="about_text_3">Chandigarh, India</p>
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default About;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./Navbar";
 import Explore from "./Explore";
 import Places from "./Places";
 import PhotoGallery from "./Photo_Gallery";
+import About from "./About";
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Explore/>
       <Places/>
       <PhotoGallery/>
+      <About/>
     </div>
   );
 }
