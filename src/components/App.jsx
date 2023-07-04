d21d8715ae2915e654b9280ce996010ff35dd33b
Mayank <myank07official@gmail.com>
2023-07-04 12:28:58 +0530

    update style.css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import Home from "./Home";
 import Navbar from "./Navbar";
 import Explore from "./Explore";
 import Places from "./Places";
+import PhotoGallery from "./Photo_Gallery";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Home/>
       <Explore/>
       <Places/>
+      <PhotoGallery/>
     </div>
   );
 }
