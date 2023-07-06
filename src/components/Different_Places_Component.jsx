8424404ab8409ab15507c15000d01d691e14217d
vivekk-tiwari <veevektiwary0109@gmail.com>
2023-07-06 10:09:35 +0530

    Update "main" to "place"

--- src/components/Different_Places_Component.jsx
+++ src/components/Different_Places_Component.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 function DifferentPlacesComponent(props) {
     return (
-        <div className="main_4">
-            <p id="main_4_text1">{props.text}</p>
-            <img src="./images/img_1.jpeg" alt="" id="main_4_img" />
+        <div className="place_4">
+            <p id="place_4_text1">{props.text}</p>
+            <img src="./images/img_1.jpeg" alt="" id="place_4_img" />
 
-            <div className="main_4_heading">
-            <p id="main_4_text2">{props.text1}</p>
-            <p id="main_4_text3">{props.text2}</p>
+            <div className="place_4_heading">
+            <p id="place_4_text2">{props.text1}</p>
+            <p id="place_4_text3">{props.text2}</p>
             </div>
         </div>
     );
--- src/components/Places_To_Visit.jsx
+++ src/components/Places_To_Visit.jsx
@@ -19,38 +19,38 @@ function PlacesToVisit(){
         </div>
 
 
-        <div className="main_2">
-            <div className="main_2_heading">
-                <img id='main_2_img' src="./images/img_1.jpeg" alt="" />
-                <p id="main_2_text">Here the radiance of nature and heart are within our reach</p>
-                <p id="main_2_text_2">The city of Chandigarh is planned to human scale,It puts us in touch with the infinte cosmos and nature</p>
-                <p id="main_2_text3">-Le Corbusier</p>
+        <div className="place_2">
+            <div className="place_2_heading">
+                <img id='place_2_img' src="./images/img_1.jpeg" alt="" />
+                <p id="place_2_text">Here the radiance of nature and heart are within our reach. </p>
+                <p id="place_2_text_2">The city of Chandigarh is planned to human scale, It puts us in touch with the infinte cosmos and nature.</p>
+                <p id="place_2_text3">-Le Corbusier</p>
             </div>
 
-            <div className="main_2_image">
-                <img src="./images/img_1.jpeg" alt="" id="main_2_img_1" />
+            <div className="place_2_image">
+                <img src="./images/img_1.jpeg" alt="" id="place_2_img_1" />
             </div>
         </div>
 
-        <div className="main_3">
-            <div className="main_3_heading">
-                <p id="main_3_text">EXHIBITS</p>
-                <p id="main_3_text_2">Solo Shows</p>
+        <div className="place_3">
+            <div className="place_3_heading">
+                <p id="place_3_text">EXHIBITS</p>
+                <p id="place_3_text_2">Solo Shows</p>
                 <ul>
                     <li id="li">Splash of Ambiguity, Grayerville Fine Art Gallery,2024</li>
                     <li id="li">Unknown Corners of Still Life, Eximus Studios,2024</li>
                     <li id="li">Paint it True,Trempleway Art Conference, 2023</li>
                 </ul>
 
-                <p id="main_3_text_3">Group Shows</p>
+                <p id="place_3_text_3">Group Shows</p>
                 <ul>
                     <li id="li">Art Maven Picks, Trempleway Art Conference,2023</li>
                     <li id="li">Trempleway Artists' Forum,Grayerville Fine Art Gallery, 2022</li>
                 </ul>
             </div>
 
-            <div className="main_3_image">
-                <img src="./images/img_1.jpeg" alt="" id="main_3_img_1" />
+            <div className="place_3_image">
+                <img src="./images/img_1.jpeg" alt="" id="place_3_img_1" />
             </div>
         </div>
     </>
