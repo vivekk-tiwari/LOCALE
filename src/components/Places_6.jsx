a7a57a5ce43af7dc5f23d6cfc8bda1affc58bbbc
samit <samitgupta32@gmail.com>
2023-07-22 15:56:09 +0530

    map api

--- src/components/Places_6.jsx
+++ src/components/Places_6.jsx
@@ -1,27 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Places6Component from './Places_6_Component';
 
-function Places6 (){
-    return (
-        <>
-        <div className="places_6_heading_1">
-            SUKHNA LAKE
-        </div>
-        <div className="places_6">
-            <Places6Component id1='places_6_img1'  id2='places_6_text'  text="Sukhna Lake "/>
-            <Places6Component id1='places_6_img1'  id2='places_6_text'  text="Sukhna Lake "/>
-            <Places6Component id1='places_6_img1'  id2='places_6_text'  text="Sukhna Lake "/>
-        </div>
 
-        <div className="places_7_heading_1">
-            ROCK GARDEN
+
+function Places6() {
+  const [mapVisible, setMapVisible] = useState(true);
+
+  const toggleMapVisibility = () => {
+    setMapVisible(!mapVisible);
+  };
+
+  return (
+    <>
+      <div className="places_6_heading_1">
+        SUKHNA LAKE
+      </div>
+
+      <div className="places_6">
+        <Places6Component id1='places_6_img1' id2='places_6_text' text="Sukhna Lake " />
+        <Places6Component id1='places_6_img1' id2='places_6_text' text="Sukhna Lake " />
+        <Places6Component id1='places_6_img1' id2='places_6_text' text="Sukhna Lake " />
+      </div>
+
+      {mapVisible && (
+        <div className="mapouter">
+          <iframe
+            style={{ width: "200%", height: "300px", marginLeft: "55px" }}
+            className="gmap_iframe"
+            width="100%"
+            
+            src="https://www.google.com/maps/embed/v1/place?q=Sukhna+Lake+Chandigarh,+Sector+1,+Chandigarh,+India&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+          ></iframe>
         </div>
-        <div className="places_7">
-        <Places6Component id1='places_6_img1' id2='places_7_text' text="Sukhna Lake "/>
-        <Places6Component id1='places_6_img1' id2='places_7_text' text="Sukhna Lake "/>
+      )}
+
+      <button className='btn' onClick={toggleMapVisibility}>map</button>
+
+      <div className="places_7_heading_1">
+        ROCK GARDEN
+      </div>
+      <div className="places_7">
+        <Places6Component id1='places_6_img1' id2='places_7_text' text="Sukhna Lake " />
+        <Places6Component id1='places_6_img1' id2='places_7_text' text="Sukhna Lake " />
+      </div>
+      
+      {mapVisible && (
+        <div className="mapouter">
+          <iframe
+            style={{ width: "200%", height: "300px", marginLeft: "55px" }}
+            className="gmap_iframe"
+            width="100%"
+            
+            src="https://www.google.com/maps/embed/v1/place?q=ROCK+GARDEN&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+          ></iframe>
         </div>
-        </>
-    );
+      )}
+
+      <button className='btn' onClick={toggleMapVisibility}>map</button>
+    </>
+  );
 }
 
 export default Places6;
--- src/components/Places_8.jsx
+++ src/components/Places_8.jsx
@@ -1,35 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Places8Component from './Places_8_Component';
 import Places6Component from './Places_6_Component';
+import "./styles.css";
 
 function Places8() {
-    return (
-        <>
-        <div className="places_8_main">
+  const [mapVisible, setMapVisible] = useState(false);
 
-            <div className="places_8_heading">
-            BIRD SANCTUARY
-            </div>
+  const toggleMapVisibility = () => {
+    setMapVisible(prevMapVisible => !prevMapVisible);
+  };
 
-            <div className="places_8_img">
-                <Places8Component/>
-                <Places8Component/>
-                <Places8Component/>
-                <Places8Component/>
-            </div>
+  return (
+    <>
+      <div className="places_8_main">
+        <div className="places_8_heading">
+          BIRD SANCTUARY
         </div>
-
-
-        <div className="places_9_heading_1">
-            JAPANESE GARDEN
+        <div className="places_8_img">
+          <Places8Component />
+          <Places8Component />
+          <Places8Component />
+          <Places8Component />
         </div>
-        <div className="places_9_main">
-            <Places6Component id1='places_9_img'/>
-            <Places6Component id1='places_9_img'/>
-            <Places6Component id1='places_9_img' />
+      </div>
+      {mapVisible && (
+        <div className="mapouter">
+          <iframe
+            style={{ width: "200%", height: "300px", marginLeft: "55px" }}
+            className="gmap_iframe"
+            width="100%"
+            src="https://www.google.com/maps/embed/v1/place?q=Bird+sanctuary,+5B,+Sector+5,+Chandigarh,+India&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+          ></iframe>
         </div>
+      )}
+
+      <button className='btn' onClick={toggleMapVisibility}> Map</button>
+
+      <div className="places_9_heading_1">
+        JAPANESE GARDEN
+      </div>
+      <div className="places_9_main">
+        <Places6Component id1='places_9_img' />
+        <Places6Component id1='places_9_img' />
+        <Places6Component id1='places_9_img' />
+      </div>
+
+      {mapVisible && (
+        <div className="mapouter">
+          <iframe
+            style={{ width: "200%", height: "300px", marginLeft: "55px" }}
+            className="gmap_iframe"
+            width="100%"
+            src="https://www.google.com/maps/embed/v1/place?q=Japanese+Garden,+near+Japanese+Park,+Sector+31+A,+Sector+31,+Chandigarh,+India&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+          ></iframe>
+        </div>
+      )}
+
+      <button className="btn" onClick={toggleMapVisibility}> Map</button>
     </>
-    );
+  );
 }
 
 export default Places8;
+
+
+
+
+
